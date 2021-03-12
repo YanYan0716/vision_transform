a8abde2d98d5f12fb478ab08696b9fcd6affd7f2
yanqian <yanqian0716@gmail.com>
2021-03-12 18:03:32 +0800

    .

---
+++ Datatset.py
@@ -0,0 +1,43 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import tensorflow as tf
+import pandas as pd
+
+
+import config
+
+
+# 制作有标签的数据集
+def label_image(img_file, label):
+    # 对图片的处理
+    img = tf.io.read_file(img_file)
+    img = tf.image.decode_jpeg(img, channels=3)
+    img = tf.image.random_flip_left_right(img)
+    img = tf.image.resize(img, (config.RESIZE, config.RESIZE))
+    img = tf.image.random_crop(img, (config.CROP, config.CROP, 3))
+    img = tf.cast(img, config.DTYPE) / 255.0
+    mean = tf.expand_dims(tf.convert_to_tensor([0.4914, 0.4822, 0.4465], dtype=config.DTYPE), axis=0)
+    std = tf.expand_dims(tf.convert_to_tensor([0.2471, 0.2435, 0.2616], dtype=config.DTYPE), axis=0)
+    img = (img-mean)/std
+
+    # 对标签的处理
+    label = tf.cast(label, dtype=config.DTYPE)
+    return {'images': img, 'labels': label}
+
+
+if __name__ == '__main__':
+
+    AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+    # 有标签的数据集 batch_size=config.BATCH_SIZE
+    df_label = pd.read_csv(config.TRAIN_PATH)
+    file_paths = df_label['file_name'].values
+    labels = df_label['label'].values
+    ds_label_train = tf.data.Dataset.from_tensor_slices((file_paths, labels))
+    ds_label_train = ds_label_train\
+        .map(label_image, num_parallel_calls=AUTOTUNE)\
+        .batch(1).shuffle(1)
+    for data in ds_label_train:
+        print(data.keys())
+        break
