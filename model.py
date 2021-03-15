c5434514efec4ac29e1b75aa11b530279c1a4829
yanqian <yanqian0716@gmail.com>
2021-03-15 17:35:14 +0800

    .

--- model.py
+++ model.py
@@ -3,6 +3,10 @@ import tensorflow.keras as keras
 from tensorflow.keras import layers
 
 
+from transformerblock import TransformerBlock
+import config
+
+
 class Encoder(layers.Layer):
     def __init__(
             self,
@@ -10,46 +14,74 @@ class Encoder(layers.Layer):
             mlp_dim,
             inputs_positions=None,
             dropout_rate=0.1,
-            train=False,
     ):
-        self.num_layers = num_layers,
-        self.mlp_dim = mlp_dim,
+        super(Encoder, self).__init__()
+        self.num_layers = num_layers
+        self.mlp_dim = mlp_dim
         self.inputs_positions = inputs_positions
-        self.dropout_rate = dropout_rate,
-        self.train = train
+        self.dropout_rate = dropout_rate
 
         self.dropout = layers.Dropout(rate=self.dropout_rate)
         self.encode_list = []
-        for layer in range(self.num_layers):
-            norm = layers.LayerNormalization(axis=-1)
-            selfAttention = layers.Attention()
+        for i in range(self.num_layers):
+            self.encode_list.append(
+                TransformerBlock(
+                    mpl_dim=self.mlp_dim,
+                    embed_size=config.HIDDEN_SIZE,
+                    num_heads=config.NUM_HEADS,
+                    forward_expansion=config.FORWARD_EXPANSION,
+                    attention_dropout_rate=config.ATTENTION_DROPOUT_RATE,
+                    dropout_rate=config.DROPOUT_RATE,
+                    out_dim=config.HIDDEN_SIZE,
+                    mask=False,
+                )
+            )
+        self.encoded = layers.LayerNormalization(axis=-1)
 
+    def call(self, inputs):
+        #addPosemb
+        pos_emb_shape = (1, inputs.shape[1], inputs.shape[2])
+        pe = tf.initializers.random_normal(stddev=0.02)(shape=pos_emb_shape)
+        inputs = tf.math.add(inputs, pe)
+
+        out = self.dropout(inputs)
+        for i in range(self.num_layers):
+            out = self.encode_list[i](out)
+        encoded = self.encoded(out)
+        return encoded
 
 
 class VisionTransformer(keras.Model):
+    """
+    for image
+    input shape: NHWC
+    """
     def __init__(
             self,
             num_classes=1000,
+            num_layers=12,
             train=False,
             resnet=None,
             patches=None,
             hidden_size=None,
-            transformer=None,
             representation_size=None,
-            classifier='gap',
+            classifier='token',
     ):
+        super(VisionTransformer, self).__init__()
         self.num_classes = num_classes
+        self.num_layers = num_layers
         self.train = train
         self.resnet = resnet
         self.patches = patches
         self.hidden_size = hidden_size
-        self.transformer = transformer
         self.representation_size = representation_size
         self.classifier = classifier
 
         if self.resnet is not None:
             # width = int(64*self.resnet.width_factor)
             print('no resnet code, please set resnet=None')
+
+        # using a conv layer to make --> B*24*24*768
         self.embedding = layers.Conv2D(
             filters=self.hidden_size,
             kernel_size=self.patches,
@@ -57,3 +89,48 @@ class VisionTransformer(keras.Model):
             padding='valid',
             name='embedding'
         )
+
+        self.encoder = Encoder(
+            num_layers=self.num_layers,
+            mlp_dim=config.MLP_DIM,
+            inputs_positions=None,
+            dropout_rate=config.DROPOUT_RATE
+        )
+
+        if self.representation_size is not None:
+            self.Dense = layers.Dense(representation_size)
+            self.tanh = keras.activations.tanh
+        self.classifier = layers.Dense(self.num_classes, kernel_initializer=keras.initializers.zeros)
+
+    def call(self, inputs, training=None, mask=None):
+        x = self.embedding(inputs)
+        n, h, w, c = x.shape
+        x = tf.reshape(x, shape=[n, h*w, c])
+        cls = tf.zeros((1, 1, c))
+        cls = tf.tile(cls, [n, 1, 1])
+        x = tf.concat([cls, x], axis=1)
+        x = self.encoder(x)
+        x = x[:, 0]
+        out = self.classifier(x)
+        return out
+
+
+def test():
+    img = tf.random.normal((1, 384, 384, 3))
+    # encoder = Encoder(num_layers=1, mlp_dim=config.MLP_DIM, inputs_positions=None, dropout_rate=0.1)
+    # out = encoder(img)
+    model = VisionTransformer(
+        num_classes=config.NUM_CLASSES,
+        num_layers=1,
+        train=False,
+        resnet=None,
+        patches=config.SIZE,
+        hidden_size=config.HIDDEN_SIZE,
+        representation_size=None,
+        classifier='token',)
+    out = model(img)
+    print(out.shape)
+
+
+if __name__ == '__main__':
+    test()
---
+++ transformerblock.py
@@ -0,0 +1,158 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as tf
+import tensorflow.keras.layers as layers
+import tensorflow.keras as keras
+
+
+import config
+
+
+class SelfAttention(layers.Layer):
+    def __init__(self, embed_size=768, heads=12, mask=False):
+        super(SelfAttention, self).__init__()
+        self.embed_size = embed_size
+        self.heads = heads
+        self.head_dim = self.embed_size // self.heads
+        self.mask = mask
+
+        assert (self.head_dim*heads == embed_size), "Embed size needs to be div by heads"
+
+        self.values = layers.Dense(self.head_dim, use_bias=False)
+        self.keys = layers.Dense(self.head_dim, use_bias=False)
+        self.queries = layers.Dense(self.head_dim, use_bias=False)
+        self.fc_out = layers.Dense(self.embed_size)
+
+    def call(self, inputs):
+        if self.mask:
+            print('please set mask=False')
+        else:
+            values = inputs
+            keys = inputs
+            query = inputs
+
+            N = query.shape[0]
+            value_len, key_len, query_len = values.shape[1], keys.shape[1], query.shape[1]
+            values = tf.reshape(values, shape=[N, value_len, self.heads, self.head_dim])
+            keys = tf.reshape(keys, shape=[N, key_len, self.heads, self.head_dim])
+            queries = tf.reshape(query, shape=[N, query_len, self.heads, self.head_dim])
+
+            values = self.values(values)
+            keys = self.keys(keys)
+            queries = self.queries(queries)
+
+            energy = tf.einsum('nqhd, nkhd->nhqk', queries, keys)
+
+            if self.mask:
+                print('please set mask False')
+
+            attention = tf.nn.softmax(energy / (self.embed_size**(1/2)), axis=3)
+            out = tf.einsum('nhql, nlhd->nqhd', attention, values)
+            out = tf.reshape(out, shape=[N, query_len, self.heads*self.head_dim])
+            out = self.fc_out(out)
+        return out
+
+
+class MlpBlock(layers.Layer):
+    def __init__(self, mlp_dim, out_dim, dropout_rate=0.1):
+        super(MlpBlock, self).__init__()
+        self.mlp_dim = mlp_dim
+        self.out_dim = out_dim
+        self.dropout_rate = dropout_rate
+
+        self.layers = keras.Sequential([
+            layers.Dense(
+                self.mlp_dim,
+                kernel_initializer=tf.initializers.glorot_normal,
+                bias_initializer=tf.initializers.random_normal(stddev=1e-6)
+            ),
+            layers.LeakyReLU(),
+            layers.Dropout(rate=self.dropout_rate),
+            layers.Dense(
+                self.out_dim,
+                kernel_initializer=tf.initializers.glorot_normal,
+                bias_initializer=tf.initializers.random_normal(stddev=1e-6)
+            ),
+            layers.Dropout(rate=self.dropout_rate),
+        ])
+
+    def call(self, inputs):
+        out = self.layers(inputs)
+        return out
+
+
+class TransformerBlock(layers.Layer):
+    def __init__(
+            self,
+            mpl_dim=3072,
+            embed_size=768,
+            num_heads=12,
+            forward_expansion=4,
+            attention_dropout_rate=0.0,
+            dropout_rate=0.0,
+            out_dim=768,
+            mask=False,
+    ):
+        super(TransformerBlock, self).__init__()
+        self.mpl_dim = mpl_dim
+        self.embed_size = embed_size
+        self.num_heads = num_heads
+        self.forward_expansion = forward_expansion
+        self.attention_dropout_rate = attention_dropout_rate
+        self.dropout_rate = dropout_rate
+        self.out_dim = out_dim
+        self.mask = mask
+
+        # self attention
+        self.SelfAttention = SelfAttention(embed_size=self.embed_size, heads=self.num_heads, mask=self.mask)
+        self.feed_forward = keras.Sequential([
+            layers.LayerNormalization(axis=-1),
+            layers.Dropout(rate=self.attention_dropout_rate),
+            layers.Dense(self.embed_size*4),
+            layers.ReLU(),
+            layers.Dense(self.embed_size),
+            layers.LayerNormalization(axis=-1),
+            layers.Dropout(rate=self.attention_dropout_rate)
+        ])
+        # mlp
+        self.norm = layers.LayerNormalization(axis=-1)
+        self.Mlpblock = MlpBlock(
+            mlp_dim=self.mpl_dim,
+            out_dim=self.out_dim,
+            dropout_rate=self.dropout_rate,
+        )
+
+    def call(self, inputs):
+        # attention
+        query = inputs
+        attention = self.SelfAttention(inputs)
+        attention = tf.math.add(attention, query)
+        out1 = self.feed_forward(attention)
+        # mlp
+        out2 = self.norm(out1)
+        out2 = self.Mlpblock(out2)
+        out = tf.math.add(out1, out2)
+        return out
+
+
+def test():
+    x = tf.random.normal((1, 24, 24, 768))
+    n, h, w, c = x.shape
+    x = tf.reshape(x, shape=[n, h*w, c])
+
+    trans = TransformerBlock(
+        mpl_dim=config.MLP_DIM,
+        embed_size=config.HIDDEN_SIZE,
+        num_heads=config.NUM_HEADS,
+        forward_expansion=config.FORWARD_EXPANSION,
+        attention_dropout_rate=config.ATTENTION_DROPOUT_RATE,
+        dropout_rate=config.DROPOUT_RATE,
+        out_dim=config.HIDDEN_SIZE,
+        mask=False,
+    )
+    out =trans(x)
+    print(out.shape)
+
+
+if __name__ == '__main__':
+    test()
